9366ca7cda7575539b02e49c10c141145ce21f9b
Tobias Barth <4nduril@gmail.com>
2019-07-23 14:00:09 +0200

    Add styling and formatting with Bootstrap

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,22 @@
 module Main exposing (main)
 
+import Bootstrap.Button as Button
+import Bootstrap.CDN as CDN
+import Bootstrap.Form as Form
+import Bootstrap.Form.Fieldset as Fieldset
+import Bootstrap.Form.Input as Input
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Table as Table
 import Browser
-import Html exposing (Html, button, div, fieldset, input, label, li, text, ul)
-import Html.Attributes exposing (for, id, type_, value)
+import Html exposing (Html, button, div, label, text)
+import Html.Attributes exposing (for, id, value)
 import Html.Events exposing (onClick, onInput)
 import Http
-import Json.Decode exposing (Decoder, field, float, map2, string)
+import Iso8601
+import Json.Decode exposing (Decoder, field, float, map2)
+import Round
+import Time exposing (Month(..), Posix, toDay, toMonth, toYear, utc)
 
 
 main =
@@ -27,6 +38,7 @@ type alias Model =
 type alias LedgerEntry =
     { amount : Amount
     , rate : Float
+    , date : Posix
     }
 
 
@@ -60,7 +72,7 @@ type Msg
 
 
 type alias RateData =
-    { date : String
+    { date : Posix
     , eurRate : Float
     }
 
@@ -72,7 +84,12 @@ update msg model =
             ( syncForeignAcc rateData model, Cmd.none )
 
         AddUSD value date ->
-            ( { model | inputAmountField = "", inputDateField = "" }, syncAddUSD value date )
+            ( { model
+                | inputAmountField = ""
+                , inputDateField = ""
+              }
+            , syncAddUSD value date
+            )
 
         SyncAddUSD ( rateData, value ) ->
             ( model
@@ -117,14 +134,19 @@ pairValueWithResult value result =
 
 rateDataDecoder : Decoder RateData
 rateDataDecoder =
-    map2 RateData (field "date" string) (field "rates" (field "EUR" float))
+    map2 RateData
+        (field "date" Iso8601.decoder)
+        (field "rates" (field "EUR" float))
 
 
 addUsdEntry : Int -> RateData -> Model -> Model
 addUsdEntry usd rateData model =
     let
         newEntry =
-            { amount = toAmount usd, rate = rateData.eurRate }
+            { amount = toAmount usd
+            , rate = rateData.eurRate
+            , date = rateData.date
+            }
     in
     { model | foreignAccount = newEntry :: model.foreignAccount }
 
@@ -133,7 +155,16 @@ addLedgerEntry : Int -> RateData -> Model -> Model
 addLedgerEntry usd rateData model =
     let
         newEntry =
-            { amount = toFloat usd * rateData.eurRate |> round |> toAmount, rate = rateData.eurRate }
+            { amount =
+                toFloat usd
+                    * rateData.eurRate
+                    -- use "half away from zero" rounding
+                    |> Round.roundNumCom 0
+                    |> round
+                    |> toAmount
+            , rate = rateData.eurRate
+            , date = rateData.date
+            }
     in
     { model | ledgerEntries = newEntry :: model.ledgerEntries }
 
@@ -157,11 +188,13 @@ syncForeignAcc rateData model =
                     |> fromAmount
                     |> toFloat
                     |> (\f -> rateData.eurRate * f)
+                    -- Use "half away from zero" rounding
+                    |> Round.roundNumCom 0
                     |> round
                     |> toAmount
 
             syncEntry =
-                { amount = subtractAmounts newLocalSaldo oldLocalSaldo, rate = rateData.eurRate }
+                { amount = subtractAmounts newLocalSaldo oldLocalSaldo, rate = rateData.eurRate, date = rateData.date }
         in
         { model | ledgerEntries = syncEntry :: model.ledgerEntries }
 
@@ -183,6 +216,27 @@ toAmount amount =
 
     else
         Cr amount
+
+
+amountToCurrencyString : Amount -> String
+amountToCurrencyString amount =
+    let
+        splittedAndReversed =
+            amountToString amount
+                |> String.split ""
+                |> List.reverse
+
+        cents =
+            List.take 2 splittedAndReversed
+                |> String.join ""
+                |> String.padLeft 2 '0'
+
+        euros =
+            List.drop 2 splittedAndReversed
+                |> String.join ""
+                |> String.padLeft 1 '0'
+    in
+    String.join "," [ euros, cents ]
 
 
 amountToString : Amount -> String
@@ -225,38 +279,70 @@ subscriptions _ =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ ul []
-            (model.ledgerEntries |> List.reverse |> List.map viewEntry)
-        , fieldset []
-            [ label [ for "amountField" ]
-                [ text "Betrag"
-                , input
-                    [ onInput UpdateAmountInput
-                    , id "amountField"
-                    , type_ "number"
-                    , value model.inputAmountField
-                    ]
-                    []
-                ]
-            , label [ for "dateField" ]
-                [ text "Datum"
-                , input
-                    [ onInput UpdateDateInput
-                    , id "dateField"
-                    , type_ "date"
-                    , value model.inputDateField
-                    ]
-                    []
-                ]
-            ]
+    Grid.container []
+        [ CDN.stylesheet
+        , viewTable model
+        , viewInputs model
         , div []
-            [ button [ onClick (submitInputField model.inputAmountField model.inputDateField) ]
+            [ text "USD: ", text (amountToString (saldo model.foreignAccount)) ]
+        ]
+
+
+viewTable : Model -> Html Msg
+viewTable model =
+    Table.table
+        { options = [ Table.striped ]
+        , thead =
+            Table.simpleThead
+                [ Table.th [] [ text "Buchungsdatum" ]
+                , Table.th [] [ text "Betrag in Euro" ]
+                , Table.th [] [ text "Wechselkurs USD -> EUR" ]
+                ]
+        , tbody =
+            Table.tbody []
+                (model.ledgerEntries |> List.reverse |> List.map viewEntry)
+        }
+
+
+viewInputs : Model -> Html Msg
+viewInputs model =
+    div []
+        [ Fieldset.config
+            |> Fieldset.asGroup
+            |> Fieldset.children
+                [ Grid.container []
+                    [ Grid.simpleRow
+                        [ Grid.col
+                            [ Col.xs12, Col.md3 ]
+                            [ Form.label [ for "amountField" ]
+                                [ text "Betrag"
+                                , Input.number
+                                    [ Input.onInput UpdateAmountInput
+                                    , Input.id "amountField"
+                                    , Input.value model.inputAmountField
+                                    ]
+                                ]
+                            ]
+                        , Grid.col
+                            [ Col.xs12, Col.md3 ]
+                            [ Form.label [ for "dateField" ]
+                                [ text "Datum"
+                                , Input.date
+                                    [ Input.onInput UpdateDateInput
+                                    , Input.id "dateField"
+                                    , Input.value model.inputDateField
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            |> Fieldset.view
+        , Grid.container []
+            [ Button.button [ Button.primary, Button.onClick (submitInputField model.inputAmountField model.inputDateField) ]
                 [ text "Submit"
                 ]
             ]
-        , div []
-            [ text "USD: ", text (amountToString (saldo model.foreignAccount)) ]
         ]
 
 
@@ -274,8 +360,62 @@ submitInputField value date =
             NoOp
 
 
-viewEntry : LedgerEntry -> Html Msg
+viewEntry : LedgerEntry -> Table.Row Msg
 viewEntry entry =
-    li []
-        [ text (amountToString entry.amount)
+    Table.tr []
+        [ Table.td []
+            [ text
+                (String.join "."
+                    [ String.fromInt (toDay utc entry.date)
+                    , posixToMonthNumberString (toMonth utc entry.date)
+                    , String.fromInt (toYear utc entry.date)
+                    ]
+                )
+            ]
+        , Table.td []
+            [ text (amountToCurrencyString entry.amount)
+            , text " €"
+            ]
+        , Table.td []
+            [ text (Round.round 5 entry.rate) ]
         ]
+
+
+posixToMonthNumberString : Time.Month -> String
+posixToMonthNumberString month =
+    case month of
+        Jan ->
+            "01"
+
+        Feb ->
+            "02"
+
+        Mar ->
+            "03"
+
+        Apr ->
+            "04"
+
+        May ->
+            "05"
+
+        Jun ->
+            "06"
+
+        Jul ->
+            "07"
+
+        Aug ->
+            "08"
+
+        Sep ->
+            "09"
+
+        Oct ->
+            "10"
+
+        Nov ->
+            "11"
+
+        Dec ->
+            "12"
